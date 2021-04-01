29a2c7476849b74cea6919c71c83c45f0f6d4eb9
May-Abo <may.abualrejal@gmail.com>
2021-04-01 14:10:27 -0400

    Make production db

--- app/models/translation.rb
+++
@@ -1,8 +0,0 @@
-class Product::Translation < ApplicationRecord
-  # extend Mobility
-
-  # belongs_to :translated_model,
-  #   class_name:  'Product',
-  #   foreign_key: :product_id,
-  #   inverse_of:  :translations
-end
